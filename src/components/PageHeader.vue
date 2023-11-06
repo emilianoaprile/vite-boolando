92d9770093122bd873fba06174828baa4f656e3b
EmilianoAprile <emilianoaprile77@icloud.com>
2023-11-06 18:11:46 +0100

    ADDED - header component: basic layout & structure

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+    name: 'PageHeader',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <div>
+        <header class="page-header">
+            <div class="container">
+                <nav class="header-navbar">
+
+                    <ul class="navbar-menu">
+                        <li class="navbar-menu-items"><a href="#">Donna</a></li>
+                        <li class="navbar-menu-items"><a href="#">Uomo</a></li>
+                        <li class="navbar-menu-items"><a href="#">Bambino</a></li>
+                    </ul>
+
+                    <div class="header-logo">
+
+                        LOGO
+
+                    </div>
+
+                    <ul class="navbar-icons">
+                        <li class="navbar-icon-items"><a href="#">Utente</a></li>
+                        <li class="navbar-icon-items"><a href="#">Preferiti</a></li>
+                        <li class="navbar-icon-items"><a href="#">Carrello</a></li>
+                    </ul>
+
+                </nav>
+            </div>
+        </header>
+
+    </div>
+</template>
+
+<style scoped>
+ul,
+li,
+menu,
+a {
+    list-style: none;
+    text-decoration: none;
+    color: white;
+}
+
+.page-header {
+    background-color: orange;
+    padding: 20px;
+}
+
+.header-navbar {
+    display: flex;
+    justify-content: space-between;
+}
+
+.header-logo,
+.navbar-menu,
+.navbar-icons {
+    display: flex;
+    align-items: center;
+}
+
+.navbar-menu-items,
+.navbar-icon-items {
+    padding: 5px 10px;
+}
+</style>
