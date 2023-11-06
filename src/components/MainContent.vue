01f5e0c0b386b915bdd233fd2fc673b9330d98df
EmilianoAprile <emilianoaprile77@icloud.com>
2023-11-06 22:14:11 +0100

    ADDED - info cards main content

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -17,7 +17,23 @@ export default {
         <div class="main-content">
             <div class="container">
                 <div class="row">
-                    <div class="col-4"></div>
+                    <div class="col-4">
+                        <div class="card">
+                            <figure>
+                                <img src="" alt="">
+                                <img src="" alt="">
+                            </figure>
+                            <span>-50%</span>
+                            <span>Sostenibilità</span>
+                            <span>&hearts;</span>
+                            <div class="product-info">
+                                <span class="name-brand">Levi's</span>
+                                <span class="info-clothes">RELAXED FIX TEE UNISEX</span>
+                                <span class="current-price">14,99€</span>
+                                <span class="previous-price"> 29,99€</span>
+                            </div>
+                        </div>
+                    </div>
                     <div class="col-4"></div>
                     <div class="col-4"></div>
                     <div class="col-4"></div>
